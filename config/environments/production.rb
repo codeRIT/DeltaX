d6c5e3e93b7cec4e3cea89a27ce53d4e21295df1
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-11-02 03:06:58 -0400

    added env for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.web_socket_server_url = ENV["ACTION_CABLE_URI"]
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
